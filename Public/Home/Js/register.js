6fb29a01fd91b402d2413698e7a3dc8e92ad5084
hongmaoxiao <buaaxhm@hotmail.com>
2014-10-12 23:39:16 +0800

    调试sms，需要下载

--- Public/Home/Js/register.js
+++ Public/Home/Js/register.js
@@ -1,18 +1,58 @@
 var timeleft = 10;
+
 function timedown( t ){
+
+var codeurl = $('#getsmscode').val();
+var codedata = "mobile=" + $("#mobile").val() + '&' + "verify=" + $('#verifycode').val();
+
 	if( timeleft == 0 ){
 		t.removeAttribute("disabled");
 		t.innerHTML="免费获取短信验证码";
 		timeleft = 10;
 	}
 	else {
+		$.ajax({
+				type: "POST",
+				dataType: "json",
+				url: codeurl,
+				data: codedata,
+				// console.log("aa");
+				// success: function (d) {
+				// 	// if (d.result == "1") {
+				// 		alert("验证码发送成功");
+				// 		// GetNumber();
+				// 		// return true;
+				// 	// }
+				// }
+
+				error: function(){
+
+        // $(this).addClass("done");
+        alert('aaadsd');
+        // alert(data);
+
+      }
+
+      // success: function(){
+
+      //   alert('data');
+
+      // }
+				// 	else {
+				// 		alert("验证码发送失败");
+				// 		return false;
+				// 		}
+				// 	}
+				});
+		// alert('aa');
 		t.setAttribute("disabled", true);
+		console.log("1");
 		t.innerHTML=""+timeleft+"秒后再次获取";
 		timeleft--;
 	setTimeout(function() {
 		timedown(t)
 		},1000);
-	}
+    }
 }
 
 $(function(){
