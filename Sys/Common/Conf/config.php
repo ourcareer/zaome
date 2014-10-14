22b8f921b2f30efde09b257290ff00334f9a07b2
ancon <woaizhongfeiyu@gmail.com>
2014-10-15 04:01:09 +0800

    上传图片

--- Sys/Common/Conf/config.php
+++ Sys/Common/Conf/config.php
@@ -190,11 +190,11 @@ return array(
         'exts'     => 'jpg,gif,png,jpeg', //允许上传的文件后缀
         'autoSub'  => true, //自动子目录保存文件
         'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
-        'rootPath' => './Data/Face/', //保存根路径
+        'rootPath' => '/Data/Face/', //保存根路径
         'savePath' => '', //保存路径
         'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
         'saveExt'  => '', //文件保存后缀，空则使用原后缀
-        'replace'  => false, //存在同名是否覆盖
+        'replace'  => ture, //存在同名是否覆盖
         'hash'     => true, //是否生成hash编码
         'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
     ), //图片上传相关配置（文件上传类配置）
