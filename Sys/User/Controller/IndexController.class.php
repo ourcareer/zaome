2d0015d271353adfc5f1eb45895635b68604fc7b
ancon <woaizhongfeiyu@gmail.com>
2014-10-15 14:40:56 +0800

    注册

--- Sys/User/Controller/IndexController.class.php
+++ Sys/User/Controller/IndexController.class.php
@@ -55,10 +55,10 @@ class IndexController extends Controller {
 			 * 测试的时候关闭
 			 */
 			
-			$verify = I('verify');
-			if(!check_verify($verify)){
-				$this->error('验证码输入错误！');
-			}
+			// $verify = I('verify');
+			// if(!check_verify($verify)){
+			// 	$this->error('验证码输入错误！');
+			// }
 			
 			/* 调用注册接口注册用户 */
             $User = new UserApi;
@@ -382,10 +382,12 @@ class IndexController extends Controller {
             $this->ajaxReturn($rt); 
         }
         if (IS_POST) {
-        	$nikename = I('nikename');
-        	$school = I('school');
-        	$username = I('username');
-        	$avatar = I('')
+        	$data['nikename'] = I('nikename');
+        	$data['school'] = I('school');
+        	$data['username'] = I('username');
+        	$data['uid'] = $uid;
+        	$User = D('user');
+        	$User->create()
         }
 
     }
