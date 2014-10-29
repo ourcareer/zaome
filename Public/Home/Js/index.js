0caffb731a612f0f2275fc5af270327daed38127
hongmaoxiao <buaaxhm@hotmail.com>
2014-10-29 20:31:55 +0800

    首页、联系我们页、团队介绍页公共样式合并

--- Public/Home/Js/index.js
+++ Public/Home/Js/index.js
@@ -1,29 +1,3 @@
-$(function(){
-	var main = $('#main-con');
-    var login = $('.login');
-    var register = $('.register');
-    var mask = $('#mask');
- $(".loginbtn").click(function(){
-    main.css('display', 'none');
-    mask.css('display','block');
-    login.css('display','block');
- })
- $("#login-close").click(function(){
-    login.css('display', 'none');
-    mask.css('display','none');
-    main.css('display','block');
- })
- $(".regbtn").click(function(){
-    main.css('display', 'none');
-    mask.css('display','block');
-    register.css('display','block');
- })
- $(".reg-close").click(function(){
-    register.css('display', 'none');
-    mask.css('display','none');
-    main.css('display','block');
- })
-});
 /*$(document).ready(function(){ 
     var Top=-426;//定义一个向上移动的距离，这个数值和你图片或DIV的高度相等 
     var Time=500;//定义一个速度 
