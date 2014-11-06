3e4ae80820ff65ace5c8979badda69569ff83666
hongmaoxiao <buaaxhm@hotmail.com>
2014-11-06 21:21:34 +0800

    矮胖问题调整（还是旧图，赵婕还没空弄新图给我）

--- Public/Home/Js/footer.js
+++ Public/Home/Js/footer.js
@@ -6,9 +6,9 @@ $(function(){
            var tar = e.srcElement||e.target;  
            if(tar.id!="qrcode"){  
                $("#qrcode span").hide();
-			   footer.css("padding","15px 0");
+			         // footer.css("padding","15px 0");
            }else{
-           		footer.css("padding","45px 0 0");
+           		// footer.css("padding","45px 0 0");
            		$('#qrcode span').show();
            }
          }  
