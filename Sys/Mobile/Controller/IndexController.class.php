8f23ab663fe0587e5b16b7b22f614ddd58c8fe37
ancon <woaizhongfeiyu@gmail.com>
2014-11-18 16:45:48 +0800

    未能自动跳转，需要调整

---
+++ Sys/Mobile/Controller/IndexController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Mobile\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+        $this->display();
+    }
+    public function share(){
+    	$this->display();
+    }
+}
